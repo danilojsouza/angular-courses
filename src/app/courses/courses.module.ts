13498ee7b4d87f23a969824cc76c5fe1f6340c66
Danilo Souza <danilosouza.bm@gmail.com>
2021-10-11 16:26:18 -0300

    Add app angular-material module

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,19 +1,17 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MaterialModule } from '../shared/material/material.module';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-
 
 @NgModule({
   declarations: [
     CoursesComponent
   ],
   imports: [
-    CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    MaterialModule
   ]
 })
+
 export class CoursesModule { }
