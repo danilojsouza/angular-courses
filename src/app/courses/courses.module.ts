c4e17e8b874ed4b3c5c4fb7b9b071b7cbb2d2170
Danilo Souza <danilosouza.bm@gmail.com>
2021-11-08 18:05:06 -0300

    Add spinner when loading courses

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,6 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '../shared/material/material.module';
 
+import { MaterialModule } from '../shared/material/material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
@@ -9,6 +10,7 @@ import { CoursesComponent } from './courses/courses.component';
     CoursesComponent
   ],
   imports: [
+    CommonModule,
     CoursesRoutingModule,
     MaterialModule
   ]
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -11,11 +11,11 @@ import { CoursesService } from './../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
-    this.courses = this.coursesService.findAll();
+    this.courses$ = this.coursesService.findAll();
   }
 
   ngOnInit(): void {
