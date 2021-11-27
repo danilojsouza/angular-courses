df098471d544d48d1b50e8a5eedfc49cebc6224c
Danilo Souza <danilosouza.bm@gmail.com>
2021-11-27 10:02:38 -0300

    Add pipe category

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { MaterialModule } from './material/material.module';
+import { CategoryPipe } from './pipes/category.pipe';
 
 @NgModule({
   declarations: [
-    ErrorDialogComponent
+    ErrorDialogComponent,
+    CategoryPipe
   ],
   imports: [
     MaterialModule,
     CommonModule
   ],
   exports: [
-    ErrorDialogComponent
+    ErrorDialogComponent,
+    CategoryPipe
   ]
 })
 export class SharedModule { }
